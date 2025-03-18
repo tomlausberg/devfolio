2cd8849cd8b85c5277b75d8bfc426ff472bc4d6e
Tom Lausberg <lausberg.tom@gmail.com>
2025-03-18 10:06:37 +0100

    add images, update .gitignore, and enhance webpack configuration

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,32 +1,54 @@
 const path = require('path');
+const fs = require('fs');  // Add this import
 const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// Create a function that returns the preprocessor function with access to fs
+const createPreprocessor = (fsModule) => {
+  return (content, loaderContext) => {
+    return content.replace(/<%= require\(['"](.+)['"]\) %>/g, (match, p1) => {
+      const filePath = path.resolve(path.dirname(loaderContext.resourcePath), p1);
+      return fsModule.readFileSync(filePath, 'utf8');
+    });
+  };
+};
 
 module.exports = {
   entry: './src/index.js',
   watch: true,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    filename: 'js/bundle.js',
   },
   module: {
     rules: [{
       test: /.s?css$/,
       use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              // options...
-            }
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader'
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            // options...
           }
-        ]
+        },
+      ]
+    },
+    {
+      test: /\.html$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'html-loader',
+        options: {
+          minimize: false,
+          preprocessor: createPreprocessor(fs)  // Pass fs to the preprocessor
+        }
+      }
     }]
   },
   optimization: {
@@ -41,5 +63,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'css/[name].bundle.css'
     }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
   ]
 };
